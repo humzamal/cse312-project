d71104da3ffe076509862a966a99ab4d3f9f5784
humzamal <humzamal@buffalo.edu>
2021-11-10 18:29:11 -0500

    1

--- projectserver.py
+++ projectserver.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from pymongo import MongoClient
+
+# Database
+my_client = MongoClient("mongodb://localhost:27017/")
+my_database = my_client["WebAppDatabase"]
+# this collection contains user accounts, and profile pictures names.
+user_accounts = my_database["UserAccounts"]
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello 312"
+def homepage():
+    with open('index.html', 'r') as index:
+        index_html = index.read()
+        index.close()
+    return index_html
 
 
 if __name__ == '__main__':
